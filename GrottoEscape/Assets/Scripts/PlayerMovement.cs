8c4ad0907d82a31c3e40816d45e4e4f58a0d680f
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2019-06-04 16:46:57 -0300

    Initial commit

---
+++ GrottoEscape/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    PlayerCollision _collision;
+    bool _canMove = true;
+
+    [Header("Stats")]
+    [SerializeField] private float _speed = 7f;
+    [SerializeField] private float _jumpForce = 12f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>(); 
+        _collision = GetComponent<PlayerCollision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Walk(horizontal);
+
+        if (Input.GetButtonDown("Jump"))
+            Jump(_jumpForce);
+    }
+
+    private void Walk(float direction) {
+        _rb.velocity = new Vector2(direction * _speed, _rb.velocity.y);
+    }
+
+    private void Jump(float jumpForce) {
+
+        if ( _collision.isOnFloor)
+            _rb.velocity = Vector2.up * jumpForce;
+    }
+}
