b2bd6f9116963241ba8c05ee4aec2fad7510c4d4
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2019-06-05 00:30:57 -0300

    Ajustes de animacao

---
+++ GrottoEscape/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator _anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void Walk(float speed)
+    {
+        _anim.SetFloat("horizontalSpeed", Mathf.Abs(speed));       
+    }
+
+    public void Jump(bool isOnFloor)
+    {
+        _anim.SetBool("isOnFloor", isOnFloor);
+    }
+
+    public void Shot(bool isShooting)
+    {
+        _anim.SetBool("shooting", isShooting);
+    }
+
+
+
+
+
+   
+}
--- GrottoEscape/Assets/Scripts/PlayerMovement.cs
+++ GrottoEscape/Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D _rb;
-    PlayerCollision _collision;
-    bool _canMove = true;
+    private Rigidbody2D _rb;
+    private PlayerCollision _collision;
+    private PlayerAnimation _animation;
+    private bool _canMove = true;
+    private bool _isOnFloor = false;
 
     [Header("Stats")]
     [SerializeField] private float _speed = 7f;
     [SerializeField] private float _jumpForce = 12f;
 
-    
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>(); 
         _collision = GetComponent<PlayerCollision>();
+        _animation = GetComponentInChildren<PlayerAnimation>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        Walk(horizontal);
 
+        float direction = Input.GetAxisRaw("Horizontal");
+        Walk(direction);
+            
         if (Input.GetButtonDown("Jump"))
             Jump(_jumpForce);
+
+        _animation.Jump(_collision.isOnFloor);
+
+        if (Input.GetButton("Fire1"))
+        {   
+            //Shot();
+            _animation.Shot(true);
+        } else if (Input.GetButtonUp("Fire1"))
+            _animation.Shot(false);
+
+
+
     }
 
     private void Walk(float direction) {
-        _rb.velocity = new Vector2(direction * _speed, _rb.velocity.y);
+
+        if (_canMove)
+            _rb.velocity = new Vector2(direction * _speed, _rb.velocity.y);
+        else
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
+
+        Flip(direction);
+        _animation.Walk(_rb.velocity.x);
     }
 
     private void Jump(float jumpForce) {
@@ -39,4 +63,18 @@ public class PlayerMovement : MonoBehaviour
         if ( _collision.isOnFloor)
             _rb.velocity = Vector2.up * jumpForce;
     }
+
+    private void Flip(float direction)
+    {
+        if ((direction > 0 && transform.localScale.x < 0) || (direction < 0 && transform.localScale.x > 0))
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);        
+    }
+
+    private void Shot()
+    {        
+        
+    }
+
+   
+
 }
