37600edbe7877c9b1e34d18b7a0109e8002761c9
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2019-06-06 13:38:00 -0300

    Meter HUD

---
+++ GrottoEscape/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static GameManager instance;
+
+    [SerializeField] private int _playerHealth = 7;    
+    public int PlayerHealth { get => _playerHealth; set => _playerHealth = value; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GrottoEscape/Assets/Scripts/Meter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meter : MonoBehaviour
+{
+    [SerializeField] private Sprite[] meters = new Sprite[7];
+    private Image _image;
+
+    void Start()
+    {
+        _image.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _image.sprite = meters[1];
+    }
+}
